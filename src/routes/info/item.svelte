706e946a3e03881c780c0cdd1bc9b5715765f34f
0xzoz <zeroexzoz@gmail.com>
2022-08-16 14:13:27 -0700

    Fix alignment issues

--- src/routes/info/item.svelte
+++ src/routes/info/item.svelte
@@ -5,19 +5,23 @@
 	const toggle = () => isOpen = !isOpen
 </script>
 <style>
-	button {border: none; background: none;display:block; color: inherit;  cursor: pointer; margin: 0; padding-bottom: 0.5em; padding-top: 0.5em}
+	div {border: none; background: none;display:block; color: inherit;  cursor: pointer; margin: 0; padding-bottom: 0.5em; padding-top: 0.5em}
     li {        
         font-family: 'Roboto', sans-serif;
-        text-align: center;
-        align-items: center;
-        margin-top:3px;
     }
+	span {
+		text-align: center !important;
+        align-items: center !important;
+		justify-content: center !important;
+		font-family: 'Roboto', sans-serif;
+	}
+
 	svg { transition: transform 0.2s ease-in;
 	}
 	
 	[aria-expanded=true] svg { transform: rotate(0.25turn); }
 </style>
-<button on:click={toggle} aria-expanded={isOpen}><svg style="tran"  width="20" height="20" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg> {entry[0]} </button>
+<div on:click={toggle} aria-expanded={isOpen}><span><svg style="tran"  width="20" height="20" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg> </span><span>{entry[0]}</span> </div>
 {#if isOpen}
 <ul transition:slide={{ duration: 300 }}>
 	{#each entry[1] as item}
