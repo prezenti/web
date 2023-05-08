6415249c8a9f81414208658445b41af0bc2bbf1f
0xzoz <zeroexzoz@gmail.com>
2023-05-08 09:37:13 -0700

    Update FAQ

--- src/routes/info/index.svelte
+++ src/routes/info/index.svelte
@@ -4,18 +4,18 @@
 	import Item from './item.svelte'
 
     const data = {
-		'Who will make grant decisions?': ["The three Prezenti stewards will make grant decisions. In some cases, if additional expertise or help is needed they may enlist help from Celo community members or subject matter experts. However, the final decision will be made by the stewards."],
+		'Who will make grant decisions?': ["The three Prezenti stewards will make grant decisions. In some cases, if additional expertise or help is needed they may enlist help from Celo community members. However, the final decision will be made by the stewards."],
 		'Who is eligible?': ["Individuals and business entities that are aligned with Celo’s mission, comply with KYC/AML policies provided by TrustID, and Terms and Conditions are eligible. If your application is accepted, we will send you the grantee agreement and the KYC/AML questionnaire."],
 		'What funding levels are available?': ["Prezenti will provide grants based on the amount needed to achieve the applicants objectives as well as the anticipated value added to the Celo ecosystem. We expect to fund a majority of projects in the 'Medium' range.", "\n", " Small: 10k cUSD or less" ,"Medium: between 10k-50k cUSD","Large: more than 50k cUSD","\n","Each applicant will create their own milestones and timeline based on their availability."],
         'What are the expected timelines?' : ["We expect the project and all milestones completed within 6 months."],
         'How does the Prezenti measure the success of the project?' : ["We expect the applicants to create their own milestones which will be used as evidence for payment release(s) for the duration of the grant. Stewards can request changes to milestones before acceptance of the application, and will have sole discretion to determine the completion of milestones after acceptance."],
-        'How is the payment handled?' : ["All grants are paid on-chain in cUSD via Celo Safe. All accepted applications will receive milestone payments by submitting evidence of completion (and having that evidence approved by a steward)."],
+        'How is the payment handled?' : ["All grants are paid on-chain in cUSD to the wallet address provided in your application. All accepted applications will receive milestone payments by submitting evidence of completion (and having that evidence approved by a steward)."],
         'How much equity will the Prezenti take?' : ["None! Prezenti is a non-profit organisation and provides grants without taking any equity. "],
-        'I have a ongoing grant from CCF1 - what happens to me now?' : ["Nothing at all. Your grant will continue to be managed by the original steward that knows and understands your project. Patrick, Dee and Deepak are committed to fulfill their current obligations until your project ends."],
-        'I have a ongoing grant from CCF1 - can I now apply for more/extra funds?' : ["You are always welcome to apply for more funds, but think carefully about the value add and actual need. This will need to be clearly articulated in your proposal and will be reviewed accordingly. We will discuss your ongoing grant with the CCF1 stewards who are still managing that contract application."],
-        'I was turned down for funding by CCF1, can I re-apply?' : ["You are always welcome to apply for funds at any time and you have honed your project ideas. But again, but think carefully about the value add and actual need. This will need to be clearly articulated in your proposal and will be reviewed accordingly."],
-        'How will the funds/projects be tracked?' : ["Milestones are managed internally. However you are are able to see completed milestones and payments on the transaction page of our website. You can also find information on grant applicants via our grants page."],
-        'I want to be a subject matter expert and support Prezenti.' : ["Great! Please do get in touch with us and we can go from there (admin@prezenti.xyz). We will be looking for brilliant people to join us to help advise and galvanize people working on Celo globally. Please note there are limited funds for these roles."]
+        'I have an ongoing grant with Prezenti, can I apply for another one?' : ["Ideally a team will successfully finish the current grant before applying for further Prezenti funds. Its worth noting that Prezenti is not a fund to bootstrap start ups during lean periods, but is a fund to boost projects working in the Celo ecosystem.", "\n", "Please note, that just because you have been successful once with Prezenti does not mean you will be successful a second time. Applications are reviewed on an ongoing basis.  "],
+        'I was turned down for funding before by Prezenti, can I now re-apply?' : ["You are always welcome to apply for funds at any time once you have honed your project ideas. But again, but think carefully about the value add and actual need. This will need to be clearly articulated in your proposal and will be reviewed accordingly."],
+        'How will the funds/projects be tracked?' : ["All active grants will be listed on our website. All other transactions (including steward’s payments) for Prezenti are available on our grants page on this website"],
+        'I want to be a subject matter expert and support Prezenti.' : ["Great! Please do get in touch with us and we can go from there (admin@prezenti.xyz). We will be looking for brilliant people to join us to help advise and galvanize people working on Celo globally. Please note there are limited funds for these roles."],
+        'Why are you no longer using Questbook to manage the grants?' : ["We were very happy to trial the use of Questbook for the management and payments of our grants when we first opened in October 2022. Questbook was in its infancy and we supported them in a number of bug fixes. Since this time it has come to light that Questbook is not currently fit for purpose for the needs of Prezenti so we are no longer using this software."]
 	}
 </script>
 
@@ -44,7 +44,7 @@
             <div  class="content-typewriter">
                 <h1>What is Prezenti</h1>
                 <p>Prezenti is a grant fund for the Celo community.</p>
-                <p>Prezenti is the second phase of the Celo Community Fund that was created with an approved spend proposal from the on-chain <a href="https://docs.celo.org/celo-codebase/protocol/proof-of-stake/epoch-rewards/community-fund">Community Fund</a>[<a href="https://explorer.celo.org/address/0xD533Ca259b330c7A88f74E000a3FaEa2d63B7972/tokens">explorer</a>]. Community members seeking grant funding can apply to Prezenti, which three community stewards manage. We provide grants of between 5000 - 30,000 $cUSD or equivalent $CELO amount. Alternatively, if you seek a higher amount, there is always the option of submitting your own <a href="https://docs.celo.org/celo-codebase/protocol/governance">governance</a> spend proposal to be voted on by CELO owners.
+                <p>Prezenti is the second phase of the Celo Community Fund that was created with an approved spend proposal from the on-chain <a href="https://docs.celo.org/celo-codebase/protocol/proof-of-stake/epoch-rewards/community-fund">Community Fund</a>[<a href="https://explorer.celo.org/address/0xD533Ca259b330c7A88f74E000a3FaEa2d63B7972/tokens">explorer</a>]. Community members seeking grant funding can apply to Prezenti, which three community stewards manage. We provide grants of between 5000 - 50,000 $cUSD or equivalent $CELO amount. Alternatively, if you seek a higher amount, there is always the option of submitting your own <a href="https://docs.celo.org/celo-codebase/protocol/governance">governance</a> spend proposal to be voted on by CELO owners.
 
                     Prezenti only supports projects that share <a href="https://celo.org/about">Celo’s Mission</a> and those that meet the evaluation criteria listed below.</p>
                     <hr/>
@@ -52,12 +52,21 @@
                     <p>Here’s the style of evaluation criteria the stewards will be considering:</p>
                     <div>
                     <ul>
+                        <li>Applications should be of a certain quality standard. We expect the application to be of a quality you would use if you were applying to receive funding for a company</li>
                         <li>Does the team understand the problem space?</li>
                         <li>Is the team capable of delivery?</li>
                         <li>Does the project proposal clearly support or promote the values of Celo?</li>
                         <li>Is the overall project proposal clear?</li>
                         <li>Are the milestones defined by the applicant specific, measurable, achievable, relevant, and time-bound?</li>
                         <li>Is the amount of funding requested reasonable?</li>
+                      </ul>
+                      
+                        <ul>
+                            <li><b>What we definitely don’t fund:</b></li>
+                            <ul>
+                                <li>Standard daily operating costs</li>
+                                <li>Projects with no emphasis on Celo and its mission  </li>
+                            </ul>
                       </ul>
                     </div>
                       <p>When an application is unsuccessful we aim to give some limited feedback addressing some of the concerns above.</p>
